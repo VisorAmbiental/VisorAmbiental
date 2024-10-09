45ba55519c2e1347e0214dc35c476b1df1055df8
Juan Genovese <juancruzgenovese22@gmail.com>
2024-10-08 22:38:28 -0300

    cache.php/Host caché

--- config/cache.php
+++ config/cache.php
@@ -66,7 +66,7 @@ return [
             ],
             'servers' => [
                 [
-                    'host' => env('MEMCACHED_HOST', '127.0.0.1'),
+                    'host' => env('MEMCACHED_HOST', 'dpg-crrc62tds78s73cuu81g-a'),
                     'port' => env('MEMCACHED_PORT', 11211),
                     'weight' => 100,
                 ],
