aa532c7874fed13e224c87605a835a9e297fb821
Juan Genovese <juancruzgenovese22@gmail.com>
2024-10-29 16:24:02 -0300

    Zoom

--- resources/js/Utils/Map/Config.js
+++ resources/js/Utils/Map/Config.js
@@ -7,7 +7,7 @@ const Config = {
   defaultStyle: () => process.env.MIX_MAP_DEFAULT_STYLE ||
     'mapbox://styles/mapbox/streets-v9',
   center: () => [-65.764424, -23.319975] || [0, 0],
-  zoom: () => process.env.MIX_MAP_ZOOM || 9,
+  zoom: () => 7.5 || 7.5,
 }
 
 export default Config
