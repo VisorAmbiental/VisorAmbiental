4b18b3f4ee780ecde6373a680e549d2f77fabf2f
Juan Genovese <juancruzgenovese22@gmail.com>
2024-10-29 16:12:17 -0300

    Lat y long hardcodeado

--- resources/js/Utils/Map/Config.js
+++ resources/js/Utils/Map/Config.js
@@ -6,8 +6,8 @@ const Config = {
   apiKey: () => process.env.MIX_MAP_ACCESS_TOKEN || '',
   defaultStyle: () => process.env.MIX_MAP_DEFAULT_STYLE ||
     'mapbox://styles/mapbox/streets-v9',
-  center: () => [process.env.MIX_MAP_CENTER_LAT, process.env.MIX_MAP_CENTER_LNG] || [0, 0],
-  zoom: () => process.env.MIX_MAP_ZOOM || 5,
+  center: () => [-23.319975, -65.764424] || [0, 0],
+  zoom: () => process.env.MIX_MAP_ZOOM || 9,
 }
 
 export default Config
