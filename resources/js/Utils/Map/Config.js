8973309726b3cc975f92eb604abc76cdadfb6853
Juan Genovese <juancruzgenovese22@gmail.com>
2024-10-29 16:17:35 -0300

    Lat y long

--- resources/js/Utils/Map/Config.js
+++ resources/js/Utils/Map/Config.js
@@ -6,7 +6,7 @@ const Config = {
   apiKey: () => process.env.MIX_MAP_ACCESS_TOKEN || '',
   defaultStyle: () => process.env.MIX_MAP_DEFAULT_STYLE ||
     'mapbox://styles/mapbox/streets-v9',
-  center: () => [-23.319975, -65.764424] || [0, 0],
+  center: () => [-65.764424, -23.319975] || [0, 0],
   zoom: () => process.env.MIX_MAP_ZOOM || 9,
 }
 
