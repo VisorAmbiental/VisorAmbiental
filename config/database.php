973519ee1da376a6b795ffecdae41b64390b8c0f
Juan Genovese <juancruzgenovese22@gmail.com>
2024-10-31 20:13:02 -0300

    Hosname eliminated

--- config/database.php
+++ config/database.php
@@ -46,7 +46,7 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', 'dpg-crrc62tds78s73cuu81g-a'),
+            'host' => env('DB_HOST', ''),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
